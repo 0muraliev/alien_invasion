911066bbe3481bb592904aec6012afd86b795b48
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2020-12-09 13:39:50 +0600

    Number bullets limited and removal old bullets added.

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,6 +30,12 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+
+            # Удаление снарядов, вышедших за край экрана.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
             self._update_screen()
 
     def _check_events(self):
@@ -62,8 +68,9 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Создание нового снаряда и включение его в группу bullets."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
