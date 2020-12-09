cdc1195215085043eabbeff20f3038a623a016a2
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2020-12-09 13:20:01 +0600

    Added ship bullets.

--- settings.py
+++ settings.py
@@ -4,9 +4,13 @@ class Settings:
     def __init__(self):
         """Инициализирует настройки игры."""
         # Параметры экрана
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (24, 32, 37)
 
         # Настройки корабля
         self.ship_speed = 1.5
+
+        # Параметры снаряда
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (240, 40, 40)
