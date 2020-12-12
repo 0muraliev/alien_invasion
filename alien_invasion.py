18b39bbc001d8898d2d7b50fe045751904d933e3
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2020-12-12 23:13:45 +0600

    Add a drop in fleet and a change in direction.

--- alien_invasion.py
+++ alien_invasion.py
@@ -81,16 +81,18 @@ class AlienInvasion:
 
     def _update_bullets(self):
         """Обновляет позиции снарядов и уничтожает старые снаряды."""
-        # Обновление позиций снарядов.
         self.bullets.update()
 
-        # Удаление снарядов, вышедших за край экрана.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
     def _update_aliens(self):
-        """Обновляет позиции всех пришельцев во флоте."""
+        """
+        Проверяет, достиг ли флот края экрана,
+        с последующим обновлением позиций всех пришельцев во флоте.
+        """
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _create_fleet(self):
@@ -120,6 +122,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien_height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Реагирует на достижение пришельцем края экрана."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Опускает весь флот и меняет направление флота."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран."""
