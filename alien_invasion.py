f11ae67417fc1552c4c5599dc61d36a2fdeb4621
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2020-12-15 23:39:06 +0600

    Reach the bottom of the screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -116,6 +116,9 @@ class AlienInvasion:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Проверить, добрались ли пришельцы до нижнего края экрана.
+        self._check_aliens_bottom()
+
     def _ship_hit(self):
         """Обрабатывает столкновение корабля с пришельцем.."""
         self.stats.ships_left -= 1
@@ -127,6 +130,14 @@ class AlienInvasion:
         self.ship.center_ship()
 
         sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        """Проверяет, добрались ли пришельцы до нижнего края экрана."""
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                self._ship_hit()
+                break
 
     def _create_fleet(self):
         """Создание флота вторжения."""
