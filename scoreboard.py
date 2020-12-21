c3b242428cd2f4b87862cf9855612eaa2168bc65
Aitmyrza Omuraliev <omuraliev2017@bk.ru>
2020-12-21 13:12:21 +0600

    Show record.

--- scoreboard.py
+++ scoreboard.py
@@ -14,21 +14,34 @@ class Scoreboard:
         # Настройки шрифта для вывода счета.
         self.text_color = (200, 200, 200)
         self.font = pygame.font.SysFont(None, 48)
-        # Подготовка исходного изображения.
+        # Подготовка изображений счетов.
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Преобразует текущий счет в графическое изображение."""
         rounded_score = round(self.stats.score, -1)
         # Вставляет запятые при преобразовании числового значения в строку
         score_str = f"{rounded_score:,}"
-        self.score_img = self.font.render(score_str, True, self.text_color, self.settings.bg_color)
+        self.score_img = self.font.render(score_str, True, self.text_color)
 
         # Вывод счета в правой верхней части экрана.
         self.score_rect = self.score_img.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Преобразует рекордный счет в графическое изображение."""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_img = self.font.render(high_score_str, True, self.text_color)
+
+        # Рекорд выравнивается по центру верхней стороны.
+        self.high_score_rect = self.high_score_img.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """Выводит счет на экран."""
         self.screen.blit(self.score_img, self.score_rect)
+        self.screen.blit(self.high_score_img, self.high_score_rect)
